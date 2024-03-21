2b16be062e9cded11b4322bca956183a1a55b63d
douglaswilkomm <164284041+douglaswilkomm@users.noreply.github.com>
2024-03-21 08:33:26 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+host = 'www.maqiatto.com';	// hostname or IP address
+port = 8883;
+topic = 'douglas.wsilva@hotmail.com/t1';		// topic to subscribe to
+topic2 = 'douglas.wsilva@hotmail.com/u1';		// topic to subscribe to
+useTLS = false;
+
+ username = "douglas.wsilva@hotmail.com";
+ password = "D.878320";
+
+
+
+cleansession = true;
